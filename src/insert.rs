77bd7a351ec787bfdcfdf170a566bb8459c98efb
Manel Naspreda <manel.naspreda@Manels-MacBook-Pro.local>
2020-10-31 12:06:41 -0300

    Put the query to insert into a rust file and adapt the up sql to not have the date columns (check it later...)

---
+++ src/insert.rs
@@ -0,0 +1,23 @@
+
+use crate::schema::users;
+use crate::models::{NewUser};
+use crate::repository::establish_connection;
+use diesel::RunQueryDsl;
+
+pub fn run() {
+    let new_user = NewUser {
+        id: &1,
+        first_name: "Manel",
+        last_name: "Naspreda",
+        phone: "+34...",
+        active: &true,
+    };
+
+    let conn = establish_connection();
+    let result = diesel::insert_into(users::table)
+        .values(&new_user)
+        .execute(&conn);
+
+    println!("{:?}", result);
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
-extern crate chrono;
-
 #[macro_use]
 extern crate diesel;
-extern crate dotenv;
 
-mod models;
-mod schema;
-mod repository;
-
-use models::{NewUser};
-use crate::diesel::RunQueryDsl;
+mod insert;
+pub mod models;
+pub mod schema;
+pub mod repository;
 
 fn main() {
-    let new_user = NewUser {
-        id: &1,
-        first_name: "Manel",
-        last_name: "Naspreda",
-        phone: "+34...",
-        active: &true,
-    };
-
-    let conn = repository::establish_connection();
-    let result = diesel::insert_into(schema::users::table)
-        .values(&new_user)
-        .execute(&conn);
-
-    println!("{:?}", result);
+    insert::run();
 }
